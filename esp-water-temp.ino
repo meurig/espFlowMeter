755b5917a473b5cf1f5776907f41db01fe5c28e4
Meurig Freeman <me@meurig.com>
2016-12-24 16:01:31 +0000

    Removed some old wifi code (was commented out)

--- esp-water-temp.ino
+++ esp-water-temp.ino
@@ -41,25 +41,6 @@ void setup(void)
 
   wifiManager.autoConnect();
   Serial.println("connected...yeey :)");
-
-/*
-  WiFi.begin(ssid, password);
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(500);
-  Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-*/
-  
 }
  
  
